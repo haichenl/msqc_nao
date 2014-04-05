3a77ad8069bebdf7c23e52a507985a319d801353
Haichen Li <haichenl@andrew.cmu.edu>
2014-04-04 21:10:50 -0400

    minor

--- @MatHF/MatHF.m
+++ @MatHF/MatHF.m
@@ -15,11 +15,12 @@ classdef MatHF < handle
             res.Int.S = res.matpsi.overlap();
             res.Int.H1 = res.matpsi.kinetic() + res.matpsi.potential();
             res.Int.H2 = zeros(nbasis, nbasis, nbasis, nbasis);
+            h2uniq = res.matpsi.tei_alluniq();
             for i = 1:nbasis
                 for j = 1:nbasis
                     for k = 1:nbasis
                         for l = 1:nbasis
-                            res.Int.H2(i,j,k,l) = res.matpsi.tei_ijkl({i, j, k, l});
+                            res.Int.H2(i,j,k,l) = h2uniq(MatHF.ij2I(MatHF.ij2I(i, j), MatHF.ij2I(k, l)));
                         end
                     end
                 end
@@ -27,7 +28,18 @@ classdef MatHF < handle
             res.HFpars.eps = 1.0e-8;
             res.HFpars.maxIter = 100;
         end
+    end
         
+    methods (Static)
+        
+        function res = ij2I(i, j)
+            if(i<j)
+                tmp = i;
+                i = j;
+                j = tmp;
+            end
+            res = i.*(i-1)./2 + j;
+        end
         
     end
 end
